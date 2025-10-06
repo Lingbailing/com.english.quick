67bce16ede4b89eb3081c177c8f2a86d67f5ff1c
Lingbailing <steve-fule@outlook.com>
2025-10-06 15:19:54 +0800

    Add files via upload

---
+++ src/pages/coloregg/__UX__FUNCTION.jsx
@@ -0,0 +1,12 @@
+import { __UX__CTX } from './coloregg.ux.coloregg_jsx';
+
+function __UX__FUNCTION() {
+let __UX__FN__CTX: typeof __UX__CTX & {};
+return (<template>
+<div class="container">
+<imgage src="/common/images/logo.png" class="egg" onclick={__UX__FN__CTX.backToHome} /></image>
+</div>);
+__UX__FN__CTX.template > __UX__FN__CTX.
+    ;
+;
+}
